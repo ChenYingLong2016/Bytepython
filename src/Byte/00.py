e0117c45670699e714efbb2ffbd8462721253f4a
ChenYingLong <1335967688@qq.com>
2016-08-24 08:36:43 +0800

    Byte
    Byte

---
+++ src/Byte/00.py
@@ -0,0 +1,15 @@
+'''
+Created on 2016年8月23日
+@author: 陈应龙
+'''
+
+age = 20
+name = 'Swaroop'
+
+'''
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('Why is {0} playing with that python?'.format(name))
+'''
+
+print('{} was {} years old when he wrote this book'.format(name, age))
+print('Why is {} playing with that python?'.format(name))
---
+++ src/Byte/01.py
@@ -0,0 +1,12 @@
+'''
+Created on 2016年8月23日
+@author: 陈应龙
+'''
+
+i = 5
+print(i)
+i += 1
+print(i)
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
